d9f5c0dc3e906efcc9e29c9c278b39412b91c9f6
samantha-tran <stran322001@gmail.com>
2022-06-28 21:07:40 +1000

    removed unnecessary files

--- info.php
+++
@@ -1,1 +0,0 @@
-<?php phpinfo(); phpinfo(INFO_MODULES); ?>
--- infs3202_quiz1/application/views/b_input.php
+++
@@ -1,8 +0,0 @@
-<hr>
-	<div class="container">
-		<div class="card w-75">
-			<div class="card-body">
-				<p class="card-text">Welcome to Task B.</p>
-			</div>
-		</div>
-	</div>
